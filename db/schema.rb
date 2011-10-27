6e813ea42b83059b095b2d2af9e8b3ba47b8d79d
Balazs.Svigruha <svigruha.balazs@hallgato.ppke.hu>
2011-10-27 21:32:21 +0200

    Validations finished and added some debug info.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111027130959) do
+ActiveRecord::Schema.define(:version => 20111027191641) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
