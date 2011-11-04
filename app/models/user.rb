67b6c2e4d9fd05516871cfee83e10a7a36f0e3f6
Balazs.Svigruha <svigruha.balazs@hallgato.ppke.hu>
2011-11-04 12:26:57 +0100

    Full implementation of password hashing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,16 +32,26 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     # Compare encrypted_password with the encrypted version of
     # submitted_password.
+    encrypted_password == encrypt(submitted_password)
   end  
   
   private
   
     def encrypt_password
+      self.salt = make_salt unless has_password?(password)
       self.encrypted_password = encrypt(password)
     end
 
     def encrypt(string)
-      string # Only a temporary implementation!
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 	
 end
