cba185dd60ff31f97216e76fc119ed54bb7ee8ae
Balazs.Svigruha <svigruha.balazs@hallgato.ppke.hu>
2011-12-01 00:10:45 +0100

    Current user fixed, tests OK.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
     self.current_user = user
   end
   
+  def current_user=(user)
+    @current_user = user
+  end
+  
   def current_user
     @current_user ||= user_from_remember_token
   end
