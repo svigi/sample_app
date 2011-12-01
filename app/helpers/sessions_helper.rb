f2f5cb8de0ce9cf6654ff909e13d29c6d3358993
Balazs.Svigruha <svigruha.balazs@hallgato.ppke.hu>
2011-12-01 15:31:54 +0100

    User must log in to edit his/her profile.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
 
   private
 
