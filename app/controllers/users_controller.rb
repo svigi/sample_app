0b3e0b4b1d02b7d2e55ef51980a6eb5803820a37
Balazs.Svigruha <svigruha.balazs@hallgato.ppke.hu>
2011-12-01 21:32:42 +0100

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_filter :authenticate, :only => [:index, :edit, :update]
+before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
 before_filter :correct_user, :only => [:edit, :update]
+before_filter :admin_user,   :only => :destroy
   
   def show
     @user = User.find(params[:id])
@@ -49,6 +50,12 @@ before_filter :correct_user, :only => [:edit, :update]
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
 
 private
 
@@ -57,7 +64,12 @@ private
   end
 
   def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end  
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end  
+  
 end
